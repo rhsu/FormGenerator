9d6348f6ecec91377686814e189ba9c35630e166
robert <rhsu123@gmail.com>
2015-11-20 13:41:00 -0800

    this now builds

---
+++ Factory/GeneralFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FormGenerator
+{
+	public class GeneralFactory
+	{
+		public Panel CreatePanel()
+		{
+			throw new NotImplementedException ("This is not implemented yet");
+		}
+
+		public Form CreateForm(IEnumerable<Panel> panels, int i)
+		{
+			throw new NotImplementedException ("This is not implemented yet");
+		}
+
+		public GeneralFactory ()
+		{
+		}
+	}
+}
+
