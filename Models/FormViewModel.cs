42ea819ac6882cb0988f4d66269983d55edf2bb6
robert <rhsu123@gmail.com>
2015-11-20 14:07:18 -0800

    backup

---
+++ Models/FormViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FormGenerator
+{
+	public class FormViewModel
+	{
+		public IEnumerable<PanelViewModel> Panels { get; }
+		public string FormName { get; }
+		public int FormId { get; }
+
+		public FormViewModel ()
+		{
+			Panels = new List<PanelViewModel> ();
+		}
+	}
+}
+
